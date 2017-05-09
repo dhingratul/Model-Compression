5eeebb6333f8171ba85615141791e555913a1659
dhingratul <atul.dhingra@rutgers.edu>
2017-05-09 09:44:38 -0400

    Documentation

--- aifi_base.py
+++ aifi_base.py
@@ -1,7 +1,15 @@
-pimport time
-start = time.perf_counter()
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May  6 20:02:25 2017
+
+@author: dhingratul
+MNIST for Experts, tensorflow: Base case for experiment
+"""
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
+import time
+start = time.perf_counter()
 mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 sess = tf.InteractiveSession()
 x = tf.placeholder(tf.float32, shape=[None, 784])
