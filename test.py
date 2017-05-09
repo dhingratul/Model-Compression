592ec9ae255ca36e06eb9d391def9e6e6155447f
dhingratul <atul.dhingra@rutgers.edu>
2017-05-09 09:25:19 -0400

    Tensorflow test program

--- test.py
+++ test.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue April  19 09:17:02 2017
+
+@author: dhingratul
+Tensorflow test program
+"""
 import tensorflow as tf
 hello = tf.constant('Hello, TensorFlow!')
 sess = tf.Session()
